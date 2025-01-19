6b8b58a920b5e29fe11caebfdbdc3de569cb2264
METIKO <temitopemetiko@gmail.com>
2025-01-19 23:07:35 +0100

    EDIT HEADER

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-dark text-white py-3">
       <div className="container">
-        <h1 className="text-center">My Portfolio</h1>
+        <h1 className="text-center">METIKO TEMITOPE OLUWAGBEMI</h1>
       </div>
     </header>
   );
