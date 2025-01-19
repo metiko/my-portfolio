29df0bc82c7abc7894466a7c09af0b40003ffd1c
METIKO <temitopemetiko@gmail.com>
2025-01-19 23:12:13 +0100

    EDIT ABT

--- src/components/about.js
+++ src/components/about.js
@@ -6,8 +6,8 @@ function About() {
       <div className="container">
         <h2 className="text-center">About Me</h2>
         <p className="text-center mt-3">
-          I am a passionate developer dedicated to creating user-friendly, innovative, and functional web applications.
-        </p>
+        Cloud and DevOps Engineer with extensive hands-on experience in supporting, automating, and optimizing deployments in both cloud and on-premise environments. Proficient in CI/CD pipeline development, container orchestration, infrastructure-as-code (IaC), and cloud platforms like AWS. 
+        Experienced in managing infrastructure, automating workflows, and improving operational efficiency through modern DevOps practices.        </p>
       </div>
     </section>
   );
