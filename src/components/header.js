1b45ccaadec52602e9fe5e52dc21eb4282185336
METIKO <temitopemetiko@gmail.com>
2025-02-16 19:00:07 +0100

    ci

--- src/components/header.js
+++ src/components/header.js
@@ -6,22 +6,70 @@ function Header() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
         <a className="navbar-brand" href="#">My Portfolio</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="about" smooth={true} duration={500}>About</Link>
+              <Link 
+                className="nav-link" 
+                to="about" 
+                smooth={true} 
+                duration={500} 
+                activeClass="active"
+                spy={true}
+                offset={-70} // Adjust to account for the fixed navbar height
+              >
+                About
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="skills" smooth={true} duration={500}>Skills</Link>
+              <Link 
+                className="nav-link" 
+                to="skills" 
+                smooth={true} 
+                duration={500} 
+                activeClass="active"
+                spy={true}
+                offset={-70}
+              >
+                Skills
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="projects" smooth={true} duration={500}>Projects</Link>
+              <Link 
+                className="nav-link" 
+                to="projects" 
+                smooth={true} 
+                duration={500} 
+                activeClass="active"
+                spy={true}
+                offset={-70}
+              >
+                Projects
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="contact" smooth={true} duration={500}>Contact</Link>
+              <Link 
+                className="nav-link" 
+                to="contact" 
+                smooth={true} 
+                duration={500} 
+                activeClass="active"
+                spy={true}
+                offset={-70}
+              >
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
